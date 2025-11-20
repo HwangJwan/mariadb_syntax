87331382546243892c7847c789cafd88013f11e4
HwangJwan <juwan7056@naver.com>
2025-11-20 16:56:21 +0900

    MariaDb_Syntax

---
+++ 1. DDL.sql
@@ -0,0 +1,77 @@
+-- mariadb 사바에 터미널 칭에서 접속(db gui툴로 접속시에는 커넥션 객체 생성하여 연결)
+mariadb -u root -p --엔터 후 비밀번호 별도 입력
+
+-- 스키마(database) 생성
+create databse board;
+
+-- 스키마 삭제
+drop database board;
+
+-- 스키마 목록 조회
+show databases;
+
+-- 스키마 선택
+use 스키마명;
+
+-- 문자 인코딩 세팅 조회 (외울필요X)
+show variables like "character_set_server"
+
+-- 문자 인코딩 변경
+alter database board default character set = utf8mb4;
+
+-- 테이블 목록조회
+show tables;
+
+-- sql문은 대문자가 관례이고, 대소문자를 구분하지는 않음
+-- 테이블명/컬럼명은 소문자가 관례이고, 대소문자가 차이가 있음
+-- 테이블 생성
+create table author(id int primary key, name varchar(255), email varchar(255), password varchar(255));
+
+-- 테이블 컬럼정보 조회
+describe author;
+
+-- 테이블 데이터 전체 조회
+select * from author;
+
+-- 테이블 생성명령문 조회(안중요)
+show create table author;
+
+-- posts 테이블 신규 생성(id, title, contents, author_id)
+create table posts(id int, title varchar(255),contents varchar(255), author_id int,  primary key(id), foreign key(author_id) references author(id));
+
+-- 테이블 제약조건 조회
+select * from information_schema.key_column_usage where table_name='posts';
+                    스키마             테이블
+
+-- 테이블 인덱스 조회 (제약조건 간겁적 조회)
+show index from posts;
+
+-- alter : 테이블의 구조 변경
+-- 테이블의 이름 변경
+alter table posts rename post;
+
+-- 테이블의 컬럼 추가
+alter table post add column age int;
+
+-- 테이블의 컬럼 삭제
+alter table drop column age;
+
+-- 테이블의 컬럼명 변경
+alter table change column contents content varchar(255);
+
+-- 테이블 컬럼의 타입과 제약조건 변경
+alter table post modify column content varchar(3000);
+alter table author modify column email varchar(255) not null unique;
+
+-- 실습1. author 테이블에 address컬럼을 추가(varchar255) name은 not null로 변경
+alter table author add column address varchar(255);
+alter table author modify column name varchar(255) not null;
+-- 실습2. post 테이블에 title을 not null로 변경, content는 contents로 이름 변경
+alter table post modify column title varchar(255) not null;
+alter table post change column content contents varchar(255);
+
+-- 테이블 삭제
+drop table abc;
+
+-- 일련의 쿼리를 실행시킬때 특정 쿼리에서 에러가 나지 않도록 if exists를 많이 사용
+drop table if exists abc;
---
+++ 2. DML.sql
@@ -0,0 +1,52 @@
+-- insert : 테이블에 데이터 삽입
+insert into 테이블명(컬럼1, 컬럼2, 컬럼3) values(데이터1, 데이터2, 데이터3);
+-- 문자열은 일반적으로 작은따옴표 ''사용
+insert into author(id, name, email) values(4, 'honggildong4', 'honggildong4@naver.com');
+
+-- update : 테이블의 데이터를 변경
+update author set name = '홍길동', email='honggildong100@naver.com' where id=3;
+
+-- delete : 삭제
+delete from 테이블명 where 조건;
+
+-- select : 조회
+select 컬럼1, 컬럼2 from 테이블명;
+select name, email from author;
+-- *은 모든 컬럼을 의미
+select * from author
+
+-- select 조건절(where) 활용
+select * from author where id=1;
+select * from author where name='홍길동';
+select * from author where id>2and name='홍길동';
+select * from author where id in (1,3,5);
+
+-- 이름이 '홍길동'인 글쓴이가 쓴 글 목록을 조회하시오
+select * from post where id in(select id from author where name='홍길동');
+
+-- 중복제거 조회 : distinct
+select distinct name from author;
+
+-- 정렬 : order by + 컬럼명
+-- asc(ascending) : 오름차순 desc(descending) : 내림차순, default는 오름차순
+-- 정렬조건 없이 조회할 경우에는 pk기준 오름차순
+select * from author order by name desc;
+
+-- 멀티컬럼 order by : 여러 컬럼으로 정렬시에, 먼저 쓴 컬럼 우선 정렬하고, 중복 시 그다음 컬럼으로 정렬적용.
+select * from author order by name desc, email asc;
+
+-- 결과값 개수 제한
+-- 가장 최근에 가입한 회원 1명만 조호할 경우
+select * from author order by id desc limit 1;
+
+-- alias(별칭)을 이용한 select
+select name as '이름', email as '이메일' from author;
+-- 아래 둘은 같은 의미
+select a.name, a.email from author as a;
+select a.name, a.email from author a;
+
+-- null을 조회 조건으로 활용   
+select * from author where password is null;
+select * from author where password is not null;
+
+-- 프로그래머스 sql 문제풀이
